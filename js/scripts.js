5f1eca96c1030e162b2c24a229a1e947212272a9
Mikah Mathews <mikah@outofband.org>
2022-01-13 11:52:54 -0800

    Add images, and pseudocode for upping count for character pick
    Co-Authored-By: Will Greenberg <greenberg.will@gmail.com>
    Co-Authored-By: Riley Shimp <rilryshimp@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 // business logic
+var fluttershy = 0;
+var krumm = 0;
+var sprongebob = 0;
+
+function addTo(input) {
+  if(input === 1){
+    spongebob = +1;
+  } else if(input === 2) {
+    krumm = +1;
+  } else if(input === 3) {
+    fluttershy = +1
+  } else {
+    return "something went wrong"
+  }
+}
+
 
 // user interface logic
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
     const name = $("#name").val();
-    const color = $("#color").val();
-    const food = $("#food").val();
-    const place = $("#place").val();
-    const animal = $("#animal").val();
+    const color = pareseInt($("#color").val());
+    const food = pareseInt($("#food").val());
+    const place = pareseInt($("#place").val());
+    const animal = pareseInt($("#animal").val());
 
     
   });
